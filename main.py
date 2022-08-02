11c3d2ab5143178366102389f51ef332ace1c89b
Gankydang <108336379+Gankydang@users.noreply.github.com>
2022-08-02 17:23:41 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-def on_button_pressed_a():
-    
-    servos.P0.run(50)
+def on_forever(a_pressed, b_pressed):
+    while True:
+        distance2 = grove.measure_in_centimeters(DigitalPin.P2)
+        if input.button_is_pressed(Button.A):
+            a_pressed2 = True
+            b_pressed2 = False
+        elif input.button_is_pressed(Button.B):
+            b_pressed2 = True
+            a_pressed2 = False
+        if a_pressed2:
+            basic.clear_screen()
+            basic.show_icon(IconNames.YES)
+            while distance2 < 10:
+                servos.P0.run(50)
+                distance2 = grove.measure_in_centimeters(DigitalPin.P2)
+            servos.P0.stop()
+        
+        elif b_pressed:
+            basic.clear_screen()
+            basic.show_icon(IconNames.NO)
+        else:
+            basic.clear_screen()
+            basic.show_icon(IconNames.NO)
 
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
-    servos.P0.set_angle(180)
-input.on_button_pressed(Button.B, on_button_pressed_b)
+b_pressed = False
+a_pressed = False
+distance = 0
+on_forever(a_pressed, b_pressed)
--- main.ts
+++ main.ts
@@ -1,6 +1,37 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    servos.P0.run(50)
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    servos.P0.setAngle(180)
-})
+function on_forever(a_pressed: boolean, b_pressed: boolean) {
+    let distance2: number;
+    let a_pressed2: boolean;
+    let b_pressed2: boolean;
+    while (true) {
+        distance2 = grove.measureInCentimeters(DigitalPin.P2)
+        if (input.buttonIsPressed(Button.A)) {
+            a_pressed2 = true
+            b_pressed2 = false
+        } else if (input.buttonIsPressed(Button.B)) {
+            b_pressed2 = true
+            a_pressed2 = false
+        }
+        
+        if (a_pressed2) {
+            basic.clearScreen()
+            basic.showIcon(IconNames.Yes)
+            while (distance2 < 10) {
+                servos.P0.run(50)
+                distance2 = grove.measureInCentimeters(DigitalPin.P2)
+            }
+            servos.P0.stop()
+        } else if (b_pressed) {
+            basic.clearScreen()
+            basic.showIcon(IconNames.No)
+        } else {
+            basic.clearScreen()
+            basic.showIcon(IconNames.No)
+        }
+        
+    }
+}
+
+let b_pressed = false
+let a_pressed = false
+let distance = 0
+on_forever(a_pressed, b_pressed)
