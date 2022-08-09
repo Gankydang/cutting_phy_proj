6d3db6155fdff8524708036f04574074b189b8d7
Gankydang <108336379+Gankydang@users.noreply.github.com>
2022-08-09 23:39:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-def on_forever(a_pressed, b_pressed):
+def on_forever():
     while True:
-        distance2 = grove.measure_in_centimeters(DigitalPin.P2)
+        
         if input.button_is_pressed(Button.A):
             a_pressed2 = True
             b_pressed2 = False
         elif input.button_is_pressed(Button.B):
             b_pressed2 = True
             a_pressed2 = False
-        if a_pressed2:
+            
+        if a_pressed2: # Activate the cutter if button a is pressed
+            distance2 = grove.measure_in_centimeters(DigitalPin.P2) # Records the distance
             basic.clear_screen()
-            basic.show_icon(IconNames.YES)
-            while distance2 < 5:
-                servos.P0.run(50)
+            basic.show_icon(IconNames.YES) # A tick indicates that cutter is activated
+            
+            while distance2 < 5: # When there is a plant blocking the sensor
+                servos.P0.run(50) # Start cutting
                 distance2 = grove.measure_in_centimeters(DigitalPin.P2)
-                pins.digital_write_pin(DigitalPin.P1, 1)
+                pins.digital_write_pin(DigitalPin.P1, 1) # While cutting, turn on the warning light
             
             servos.P0.stop()
             pins.digital_write_pin(DigitalPin.P1, 0)
 
-        elif b_pressed:
+        elif b_pressed: # Deactivate the cutter if button b is pressed
             basic.clear_screen()
-            basic.show_icon(IconNames.NO)
+            servos.P0.stop()
+            basic.show_icon(IconNames.NO) # A cross indicates that the cutter is deactivated
         else:
             basic.clear_screen()
             basic.show_icon(IconNames.NO)
 
-b_pressed = False
-a_pressed = False
-distance = 0
-on_forever(a_pressed, b_pressed)
+on_forever()
