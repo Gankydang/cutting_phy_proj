b8fb7c210a1d015c4c1fe517016971b000179a9d
Gankydang <108336379+Gankydang@users.noreply.github.com>
2022-08-12 18:06:11 +0800

    Create old_code.py

---
+++ old_code.py
@@ -0,0 +1,35 @@
+Moisture = 0
+distance2 = 0
+a_pressed = False
+b_pressed = False
+def on_forever():
+    global Moisture, distance2
+    while True:
+        Moisture = pins.analog_read_pin(AnalogPin.P2)
+        distance2 = grove.measure_in_centimeters_v2(DigitalPin.P0)
+        if input.button_is_pressed(Button.A):
+            a_pressed = True
+            b_pressed = False
+        elif input.button_is_pressed(Button.B):
+            b_pressed = True
+            a_pressed = False
+        if a_pressed:
+            if distance2 < 3.3:
+                basic.show_icon(IconNames.HAPPY)
+            else:
+                basic.show_icon(IconNames.SAD)
+
+            if Moisture < 30:
+                pins.digital_write_pin(DigitalPin.P1, 1)
+            else:
+                pins.digital_write_pin(DigitalPin.P1, 0)
+                
+            
+        elif b_pressed:
+            basic.clear_screen()
+            pins.digital_write_pin(DigitalPin.P1, 0)
+        
+
+on_forever()
+
+
